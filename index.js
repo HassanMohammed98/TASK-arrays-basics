3b0bfd853cd231a2c7fe7195ad3ae1de2411fab9
Hassan Mohammed <hassan-n98@hotmail.com>
2022-01-10 18:08:33 +0300

    done task 1 10/01/2022

---
+++ index.js
@@ -0,0 +1,12 @@
+let emptyArray = [];
+let hobbies = ["coding","drawing","watching movies","swimming","going out","learning new techs"];
+let numbers = [1,2,3,12,20];
+console.log(numbers[1]);
+console.log(hobbies[hobbies.length-1]);
+hobbies.push("shopping");
+console.log(hobbies[hobbies.length-1]);
+console.log(hobbies.length);
+hobbies.splice(-2);
+console.log(hobbies.length);
+console.log(hobbies);
+hobbies = [];
